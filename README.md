fe9b12a1c3048cc12caf2fa91d0e12e96b9d8647
sungjine <sungjine456@gamil.com>
2025-03-14 16:35:39 +0900

    project settings 변경경

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # asset-management
+
+### 개발도구
+
+- Server
+  JAVA 8
+  Spring Boot 2.7
+  H2
+
+- Client
+  React
--- server/src/main/java/com/psp/am/AmApplication.java
+++ server/src/main/java/com/psp/am/AmApplication.java
@@ -2,12 +2,19 @@ package com.psp.am;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmApplication.class, args);
 	}
 
+	@GetMapping("/test")
+	public String test() {
+		return "hello";
+	}
 }
