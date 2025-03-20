4691b01bb30bc27b890e2849760a0fbf6e856673
sungjine456 <sungjine456@gmail.com>
2025-03-20 12:25:30 +0900

    디자인 추가 개선 및 에러 수정

--- client/src/components/pages/settings/StockList.tsx
+++ client/src/components/pages/settings/StockList.tsx
@@ -15,28 +15,35 @@ function StockList() {
   return (
     <>
       <h3>등록된 주식 목록</h3>
-      <div>
-        <table className="m-auto">
-          <thead>
-            <th>종목 코드</th>
-            <th>종목 이름</th>
-          </thead>
-          <tbody>
-            {list.map((item) => {
-              return (
-                <tr>
-                  <td>{item.code}</td>
-                  <td>{item.name}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className="buttons">
-        <Link to="/settings/stock/add" className="btn">
-          추가
-        </Link>
+      <div className="d-flex-column" style={{ alignItems: "center" }}>
+        <div className="d-flex-column">
+          <table>
+            <thead>
+              <tr>
+                <th>종목 코드</th>
+                <th>종목 이름</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((item) => {
+                return (
+                  <tr key={item.code}>
+                    <td>{item.code}</td>
+                    <td>{item.name}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          <Link
+            to="/settings/stock/add"
+            className="btn"
+            style={{ alignSelf: "flex-end", marginTop: "10px" }}
+          >
+            추가
+          </Link>
+        </div>
       </div>
     </>
   );
